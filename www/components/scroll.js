1b276f770f90851c1904f4d23730ec975f21f946
Peckins <messias.morais@etec.sp.gov.br>
2019-04-19 10:32:00 +0900

    modificação de cores e incrementação de um mapa temporário

---
+++ www/components/scroll.js
@@ -0,0 +1,12 @@
+// This is a JavaScript file
+
+/*var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("toolbar").style.top = "0";
+  } else {
+    document.getElementById("toolbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}*/
